f6e743351c3a8986e5d70005d1b39289438d57af
Justin Cook <justin.cook@quotewright.com>
2021-04-05 19:08:17 -0400

    add add-block plugin

---
+++ site/web/app/plugins/add-block/src/block/block.js
@@ -0,0 +1,72 @@
+/**
+ * BLOCK: single-block
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+//  Import CSS.
+import './style.scss';
+import './editor.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { InnerBlocks } = wp.blockEditor;
+const TEMPLATE = [
+	['core/heading', { placeholder: 'Recipe Title' }],
+	['core/columns', {},[
+		['core/column', {}, [
+			['core/image']
+		]],
+		['core/column', {},[
+			['core/paragraph', { placeholder: 'Enter short recipe description...' }],
+			['core/paragraph', { placeholder: 'Enter ingredients...' }],
+			['core/button', { text: 'Make this Recipe' }],
+			]
+		]
+	]
+]];
+
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @param  {string}   name     Block name.   
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'cgb/block-single-block', {
+	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	title: __( 'single-block - CGB Block', 'CGB' ), // Block title.
+	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	keywords: [
+		__( 'single-block — CGB Block' ),
+		__( 'CGB Example' ),
+		__( 'create-guten-block' ),
+	],
+
+	// The "edit" property must be a valid function.
+	edit: function( props ) {
+          const { className } = props;
+		// Creates a <p class='wp-block-cgb-block-single-block'></p>.
+		return (
+		<div className={className}>
+			<InnerBlocks template={TEMPLATE} />
+		</div>
+		);
+	},
+
+	// The "save" property must be specified and must be a valid function.
+	save: function( props ) {
+		return (
+		<div>
+			<InnerBlocks.Content />
+		</div>
+		);
+	},
+} );
